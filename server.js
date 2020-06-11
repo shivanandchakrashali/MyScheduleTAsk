3ab44805159547f66c93cb894318628a4fe99f0d
shivanand <shivanandchakrashali@gmail.com>
2020-06-12 01:02:29 +0530

    serverfile

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var express=require('express');
 var app=express();
-var mongojs=require('mongojs');
 var cron = require('node-cron');
- 
-
-
-
-var db=mongojs('Practice',['testScript']);
-
 var bodyParser=require('body-parser');
  
  
@@ -24,14 +17,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
- var task =''
-console.log("wwsssssss")
+ 
 app.post("/setschedule",function(req,res){
   
   
    if(req.body.time == 'Seconds'){
     cron.schedule('* * * * * *', () => {
       var  date = new Date()
+      console.log('FOR EVERY SECONDS')
          console.log(date.getHours()+':'+date.getMinutes()+':'+date.getSeconds())
       })
    
@@ -39,12 +32,14 @@ app.post("/setschedule",function(req,res){
    else if(req.body.time == 'Minute'){
      cron.schedule('* * * * *', () => {
       var  date = new Date()
+      console.log('FOR EVERY MINUTE')
       console.log(date.getHours()+':'+date.getMinutes()+':'+date.getSeconds())
     })
    }
    else if(req.body.time == 'Hour'){
     cron.schedule('* * * *', () => {
       var  date = new Date()
+      console.log('FOR EVERY HOUR')
       console.log(date.getHours()+':'+date.getMinutes()+':'+date.getSeconds())
     })
 }
@@ -52,60 +47,7 @@ app.post("/setschedule",function(req,res){
      
 
 })
-// app.get("/anand",function(req,res){
-//     console.log("jjjjjjjjjjjjjjjjjjjjjjjjjj")
-//     db.usersData.find(function(err,doc){
-//         if(err){return err}
-//         console.log(doc);
-//         res.json(doc)
-//     })
-     
-
-// }) 
-
-
-// app.get("/getBase",function(req,res){
-//     console.log("jjjjjjjjjjjjjjjjjjjjjjjjjj")
-//     db.usersData.find(function(err,doc){
-//         if(err){return err}
-//         console.log(doc);
-//         res.json(doc)
-//     })
-     
-
-// }) 
-
-app.post("/storeUsers1",function(req,res){
-    console.log(req.body)
-    // db.usersData.insert({"Name":req.body.userName,"Repository":req.body.repository,"Url":req.body.url},function(err,doc){
-    //     if(err){return err}
-    //     console.log(doc);
-    //     res.json(doc)
-    // })  
-
-})
-//     app.post("/UpdateUsers",function(req,res){
-//         console.log(req.body.id)
-//         db.usersData.update({"_id":mongojs.ObjectId(req.body.id)},
-
-//            {$set:{"Name":req.body.name123,"Repository":req.body.Repository,"Url":req.body.Url}},function(err,doc){
-//             if(err){return err}
-//             console.log(doc);
-//             res.json(doc)
-//         })
-     
-
-// })
-
-// app.get("/findName:name",function(req,res){
-    
-//     db.usersData.find({"Name":req.params.name},function(err,doc){
-//         if(err){return err}
-//         console.log(doc);
-//         res.json(doc)
-//     })
-// })
-
+ 
 const port=5555;
 app.listen(port,function(){
     console.log("Server is Running on "+" "+port)
